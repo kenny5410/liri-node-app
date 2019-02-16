fbb49bb862f19a00a23e56d9cbe355fc45c7525f
Kenny <nels5410@gmail.com>
2019-02-16 16:15:31 -0600

    first commit

---
+++ liri.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+
+var spotify = new spotify(keys.spotify);
+
+var command = process.argv[2];
+
+switch (command) {
+    case:
+    //concert-this
+    //spotify-this-song
+        //Artist
+        //The song's name
+        //A preview link of the song from Spotify
+        //The album that the song is from
+
+    //movie-this
+    //do-what-it-says
+} 
